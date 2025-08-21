bceb165309d2a521caaaefc44d310d10d90ec1e2
Aravindh-29 <aravindhkumar146@gmail.com>
2025-08-21 07:42:52 +0530

    final changes for v1.0 relese

--- program.cs
+++ program.cs
@@ -6,7 +6,7 @@ namespace DotNetApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Login enabled!!");
+            Console.WriteLine("Successful App V1.0");
         }
     }
 }
